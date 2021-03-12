a1694ff9eab5e9c6c0df8443de85b9d1aa6b43be
devrock20 <satyadev9691@gmail.com>
2021-03-12 11:12:05 -0500

    Minion file changes

--- shared_memory/activity-minions/minions.cpp
+++ shared_memory/activity-minions/minions.cpp
@@ -1,28 +1,34 @@
 #include <iostream>
 #include  <thread>
+#include <string>
+#include <vector>
+
+using namespace std;
 
 void minion(int m){
-	std::cout << "Hello! I am minion "<< m
+  cout << "Hello! I am minion "<< m << "\n";
 }
 
 
 int main (int argc, char** argv) {
   if (argc < 2) {
-    std::cerr<<"usage: "<<argv[0]<<" <nbminions>\n";
+    cerr<<"usage: "<<argv[0]<<" <nbminions>\n";
     return -1;
   }
   float m = stoi(argv[0]);
-  std::vector <std::thread > minion_threads;
+  vector <std::thread > minion_threads;
   for (int i=0; i<m; ++i) 
   	{ 
-  		std:: thread  minion_thread (minion,i);
+  		thread  minion_thread (minion,i);
   		minion_threads.push_back(std::move(minion_thread ));
   	}
   for (auto & t : minion_threads)
    {
    	t.join ();
   }
-  
+  thread overlord_thread ("hello minions! I am the Overlord!")
+  thread.join()
+
   
   return 0;
 }
