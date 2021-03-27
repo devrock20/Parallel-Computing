7f8e5151d656a4ed8aa9b32cdae52fb84f3e49ae
Satyadev Tummala <stummal5@mba-i1.uncc.edu>
2021-03-26 23:06:22 -0400

    Using namespace std

--- activity-hashtable-coarse/coarse_grain/main.cpp
+++ activity-hashtable-coarse/coarse_grain/main.cpp
@@ -130,5 +130,7 @@ int main(int argc, char **argv)
   // Do not touch this, need for test cases
   std::cout << ht.get(testWord) << std::endl;
 
+  std::cerr << time_elapsed.count()<<"\n";
+
   return 0;
 }
