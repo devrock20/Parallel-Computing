59217bdf320ada7109d8ea48b7a930158c010199
Akhil Reddy Gangula <akhilreddy.gangula96@gmail.com>
2021-04-05 19:45:55 -0400

    Fine grain commit

---
+++ activity-hashtable-coarse/fine_grain/HashTable.h
@@ -0,0 +1,139 @@
+#ifndef HASH_MAP_H_
+#define HASH_MAP_H_
+
+#include <cstdint>
+#include <iostream>
+#include <functional>
+#include <mutex>
+#include <shared_mutex>
+// #include "HashNode.h"
+using namespace std;
+
+// mutex wrapper since creating vector of shared_time_mutex was not possible.
+class mutex_wrapper
+{
+public:
+    mutable shared_timed_mutex mu;
+};
+
+//Node that holds key and value
+template <typename K, typename V>
+class HashNode
+{
+public:
+    HashNode(K key_, V value_) : next(nullptr), key(key_), value(value_)
+    {
+    }
+    ~HashNode()
+    {
+        next = nullptr;
+    }
+
+    const K &getKey() const { return key; }
+    void setValue(V value_) { value = value_; }
+    const V &getValue() const { return value; }
+
+    HashNode *next;
+
+private:
+    K key;
+    V value;
+};
+
+//bucket which holds linked list of HashNodes whose hash of key collided.
+template <typename K, typename V>
+class HashBucket
+{
+public:
+    HashBucket() : head(nullptr)
+    {
+    }
+
+    V find(const K &key, shared_timed_mutex &mutex_) const
+    {
+        shared_lock<shared_timed_mutex> lock(mutex_);
+        HashNode<K, V> *node = head;
+
+        while (node != nullptr)
+        {
+            if (node->getKey() == key)
+            {
+                return node->getValue();
+            }
+            node = node->next;
+        }
+        return 0;
+    }
+
+    void update(const K &key, shared_timed_mutex &mutex_)
+    {
+        unique_lock<shared_timed_mutex> lock(mutex_);
+        HashNode<K, V> *prev = nullptr;
+        HashNode<K, V> *node = head;
+
+        while (node != nullptr && node->getKey() != key)
+        {
+            prev = node;
+            node = node->next;
+        }
+
+        if (nullptr == node)
+        {
+            if (nullptr == head)
+            {
+                head = new HashNode<K, V>(key, 1);
+            }
+            else
+            {
+                prev->next = new HashNode<K, V>(key, 1);
+            }
+        }
+        else
+        {
+            int count = node->getValue();
+            node->setValue(++count);
+        }
+    }
+
+private:
+    HashNode<K, V> *head;
+};
+
+
+template <typename K, typename V, typename F = std::hash<K>>
+class HashTable
+{
+public:
+    HashTable(size_t hashSize_ = 1031) : hashSize(hashSize_)
+    {
+        hashTable = new HashBucket<K, V>[hashSize];
+        mu_wrapper = new mutex_wrapper[256];
+    }
+
+    HashTable(const HashTable &) = delete;
+    HashTable(HashTable &&) = delete;
+    HashTable &operator=(const HashTable &) = delete;
+    HashTable &operator=(HashTable &&) = delete;
+
+    V get(const K &key) const
+    {
+        size_t hashValue = hashFn(key) % hashSize;
+        V res = hashTable[hashValue].find(key, mu_wrapper[hashValue % MAX_MUTEX].mu);
+        return res;
+    }
+
+    void update(const K &key)
+    {
+        size_t hashValue = hashFn(key) % hashSize;
+        hashTable[hashValue].update(key, mu_wrapper[hashValue % MAX_MUTEX].mu);
+    }
+
+private:
+    HashBucket<K, V> *hashTable;
+    F hashFn;
+    const size_t hashSize;
+    size_t MAX_MUTEX = 256;
+    mutex_wrapper *mu_wrapper;
+};
+
+#endif
--- activity-hashtable-coarse/fine_grain/main.cpp
+++ activity-hashtable-coarse/fine_grain/main.cpp
@@ -2,124 +2,122 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <thread>
+#include <mutex>
 
-#include "Dictionary.hpp"
-#include "MyHashtable.hpp"
+#include "HashTable.h"
+using namespace std;
 
 //Tokenize a string into individual word, removing punctuation at the
 //end of words
-std::vector<std::vector<std::string>> tokenizeLyrics(const std::vector<std::string> files) {
-  std::vector<std::vector<std::string>> ret;
+vector<vector<string>> tokenizeLyrics(const vector<string> files)
+{
+  vector<vector<string>> ret;
 
-  for(auto filename : files) {
-    //std::cout<<"reading "<<filename<<"\n";
-    std::vector<std::string> my_vect;
-    std::ifstream in (filename);
+  for (auto filename : files)
+  {
+    //cout<<"reading "<<filename<<"\n";
+    vector<string> my_vect;
+    ifstream in(filename);
 
-    std::string line;
+    string line;
 
     //For each line
-    while (getline(in, line, '\n')) {
+    while (getline(in, line, '\n'))
+    {
       //Skip all like starting with [
       if (line[0] == '[')
         continue;
 
-      std::stringstream ssline (line);
+      stringstream ssline(line);
       //For all words
-      while (ssline) {
-        std::string word;
+      while (ssline)
+      {
+        string word;
         ssline >> word;
-        if (ssline) {
+        if (ssline)
+        {
           //remove punctuation at the end of word
-          while (word.length() > 0
-                 && std::ispunct(word[word.length() - 1])) {
+          while (word.length() > 0 && ispunct(word[word.length() - 1]))
+          {
             word.pop_back();
           }
           my_vect.push_back(word);
         }
       }
     }
-    //std::cout<<"read "<<my_vect.size()<<" words\n";
+    //cout<<"read "<<my_vect.size()<<" words\n";
     ret.push_back(my_vect);
   }
   return ret;
 }
 
 void hashtable_populate(vector<string> filecontent,
-                        Dictionary<string, int>& dict){
-  
-  
-    for (auto & w : filecontent) {
-      int count = dict.get(w);
-      ++count;
-      dict.set(w, count);
-    }
+                        HashTable<string, int> &dict)
+{
+
+  for (auto &w : filecontent)
+  {
+    dict.update(w);
+  }
 }
-
-
 
 int main(int argc, char **argv)
 {
-  if (argc < 4) {
-    std::cerr<<"usage: ./main <sources> <testword> <threshold>"<<std::endl;
+  if (argc < 4)
+  {
+    cerr << "usage: ./main <sources> <testword> <threshold>" << endl;
     return -1;
   }
 
   // Parse Command Line
-  std::string source = argv[1];
-  std::string testWord = argv[2];
-  int32_t thresholdCount = std::stoi(argv[3]);
+  string source = argv[1];
+  string testWord = argv[2];
+  int32_t thresholdCount = stoi(argv[3]);
 
   // Obtain List of Files
-  std::vector<std::string> files;
-  std::ifstream in (source);
-  std::string line;
-  while (getline(in, line, '\n')) {
+  vector<string> files;
+  ifstream in(source);
+  string line;
+  while (getline(in, line, '\n'))
+  {
     files.push_back(line);
   }
 
   // Tokenize Lyrics
   auto wordmap = tokenizeLyrics(files);
 
-  MyHashtable<std::string, int> ht;
-  Dictionary<std::string, int>& dict = ht;
-
-
-
-  // write code here
+  HashTable<string, int> hm;
 
   vector<thread> filethreads;
-  auto start =chrono::steady_clock::now();
-  for (int i =0;i<wordmap.size();i++){
-    filethreads.push_back(thread  (hashtable_populate,wordmap.at(i),ref(ht)));
+  auto start = chrono::steady_clock::now();
+  for (int i = 0; i < wordmap.size(); i++)
+  {
+    filethreads.push_back(thread(hashtable_populate, wordmap.at(i), ref(hm)));
   }
 
-  for (auto &t: filethreads){
+  for (auto &t : filethreads)
+  {
     t.join();
   }
 
   auto stop = chrono::steady_clock::now();
-  chrono::duration<double> time_elapsed = stop-start;
+  chrono::duration<double> time_elapsed = stop - start;
 
+  // write code here
 
-
-
-
-
-
-
-
-
-  // Check Hash Table Values 
-  /* (you can uncomment, but this must be commented out for tests)
-  for (auto it : dict) {
-    if (it.second > thresholdCount)
-      std::cout << it.first << " " << it.second << std::endl;
-  }
-  */
+  // Check Hash Table Values
+  // (you can uncomment, but this must be commented out for tests)
+  // for (auto it : dict) {
+  //   if (it.second > thresholdCount)
+  //     cout << it.first << " " << it.second << endl;
+  // }
+  
 
   // Do not touch this, need for test cases
-  std::cout << ht.get(testWord) << std::endl;
+  cout << hm.get(testWord) << endl;
+  
+  cerr << time_elapsed.count() << "\n";
 
   return 0;
 }
