071d9f6b3d319b68d45f27a0d83980ae7bb68e87
Akhil Reddy Gangula <akhilreddy.gangula96@gmail.com>
2021-04-24 14:52:04 -0400

    static parfor - checkpoint

--- parallelLoops/static/seq_loop.hpp
+++ parallelLoops/static/seq_loop.hpp
@@ -2,8 +2,20 @@
 #define __SEQ_LOOP_H
 
 #include <functional>
+#include <vector>
+#include <thread>
 
-class SeqLoop {
+using namespace std;
+struct my_collection
+{
+  float a, b;
+  int n, end, start, functionid, intensity;
+
+  float sum;
+};
+
+class SeqLoop
+{
 public:
   /// @brief execute the function f multiple times with different
   /// parameters possibly in parallel
@@ -11,9 +23,12 @@ public:
   /// f will be executed multiple times with parameters starting at
   /// beg, no greater than end, in inc increment. These execution may
   /// be in parallel
-  void parfor (size_t beg, size_t end, size_t inc,
-	       std::function<void(int)> f) {
-    for (size_t i=beg; i<end; i+= inc) {
+  void parfor1(size_t beg, size_t end, size_t inc,
+               std::function<void(int)> f)
+  {
+
+    for (size_t i = beg; i < end; i += inc)
+    {
       f(i);
     }
   }
@@ -35,20 +50,56 @@ public:
   ///
   /// Once the iterations are complete, each thread will execute after
   /// on the TLS object. No two thread can execute after at the same time.
-  template<typename TLS>
-  void parfor (size_t beg, size_t end, size_t increment,
-	       std::function<void(TLS&)> before,
-	       std::function<void(int, TLS&)> f,
-	       std::function<void(TLS&)> after
-	       ) {
+  template <typename TLS>
+  void parfor(size_t beg, size_t end, size_t increment, size_t nthreads, std::function<void(TLS &)> before,
+              std::function<void(int, TLS &)> func,
+              std::function<void(TLS &)> after)
+  {
     TLS tls;
-    before(tls);    
-    for (size_t i=beg; i<end; i+= increment) {
-      f(i, tls);
+    vector<thread> thread_stack;
+    // float localSum[nthreads];
+    vector<TLS> localSum(nthreads);
+    int flag = true;
+    //0 2 4 6 8
+    //1 3 5 7 9
+    // beg= 0
+    // nthreads = 2
+    for (int t = 0; t < nthreads; t += 1)
+    {
+      before(localSum[t]);
+      // localSum[t] = 0.0;
+      // for(int j=t;j<end;j+=nthreads) {
+      //   func(j, localSum[t]);
+      //   // cout<< localSum[t] <<endl;
+      // }
+      // parfor1(t, end, nthreads, [&](int j) -> void {
+      //       /**
+      //        * problem here is localSum when refernce outside this lambda function is giving empty array
+      //       */
+      //       func(j, localSum[t]);
+      //       // cout << localSum[t] << endl; //this gets printed correctly
+      //     });
+
+      auto healperFn = [&](int j) -> void {
+        /**
+             * problem here is localSum when refernce outside this lambda function is giving empty array
+            */
+        func(j, localSum[t]);
+        cout << localSum[t] << " t: " << t << endl; //this gets printed correctly
+      };
+      thread_stack.push_back(thread(&SeqLoop::parfor1, this, t, end, nthreads, healperFn));
     }
-    after(tls);
+    for (auto &itr : thread_stack)
+    {
+      itr.join();
+    }
+    for (auto &itr : localSum)
+    {
+      cout << itr << endl; // when accessing same array outside the lambda, I am getting 0
+      after(itr);
+      // tls += itr;
+    }
   }
-  
 };
 
 #endif
--- parallelLoops/static/static_sched.cpp
+++ parallelLoops/static/static_sched.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
@@ -5,9 +6,9 @@
 #include <chrono>
 #include <cmath>
 #include "seq_loop.hpp"
-#include "thread"
-#include <vector>
+#include "vector"
 using namespace std;
+
 #ifdef __cplusplus
 extern "C"
 {
@@ -21,59 +22,6 @@ extern "C"
 #ifdef __cplusplus
 }
 #endif
-
-//abstract funtion
-typedef float (*absFun)(float x, int intensity);
-SeqLoop sl;
-
-void single_thread_action(int i, float n, int nbthreads, float a, float b, absFun function, float intensity, float &sum)
-{
-
-  sl.parfor<float>(
-      i, n, nbthreads,
-      [&](float &localSum) -> void {
-        localSum = 0.00;
-      },
-      [&](int count, float &localSum) -> void {
-        float expression = (a + (count + 0.5) * ((b - a) / n));
-        float integral = function(expression, intensity);
-        localSum += integral;
-      },
-      [&](float &localSum) -> void {
-        sum += localSum;
-      });
-}
-
-float getSummation(absFun function, float a, float b, float n, float intensity, int nbthreads)
-{
-  float sum = 0.000;
-  vector<thread> filethreads;
-  sl.parfor(
-      0, nbthreads, 1,
-      [&](int i) -> void {
-        filethreads.push_back(thread(single_thread_action, i, n, nbthreads, a, b, function, intensity, ref(sum)));
-//         filethreads.at(i).join();
-      });
-  for(auto &it: filethreads) {
-    it.join();
-  }
-  return sum;
-}
-
-// calculates and displays the integral
-void calcAndDisplayIntegral(float sum, float a, float b, float n)
-{
-  cout << ((b - a) / n) * sum << endl;
-}
-
-// calculates and displays time difference
-void calcAndDisplayTime(chrono::time_point<chrono::system_clock> before_integration)
-{
-  auto after_integration = chrono::system_clock::now();
-  float time_duration = chrono::duration_cast<chrono::milliseconds>(after_integration - before_integration).count();
-  float time_diff_sec = time_duration / 60;
-  cerr << time_diff_sec;
-}
 
 int main(int argc, char *argv[])
 {
@@ -89,32 +37,45 @@ int main(int argc, char *argv[])
   float n = stoi(argv[4]);
   float intensity = stoi(argv[5]);
   int nbthreads = stoi(argv[6]);
-  // int functionid = 1;
-  // float a = 0;
-  // float b = 10;
-  // float n = 1000;
-  // float intensity = 1;
-  // int nbthreads = 2;
-  float summation;
-  auto before_integration = chrono::system_clock::now();
-  switch (functionid)
-  {
-  case 1:
-    summation = getSummation((absFun)f1, a, b, n, intensity, nbthreads);
-    break;
-  case 2:
-    summation = getSummation((absFun)f2, a, b, n, intensity, nbthreads);
-    break;
-  case 3:
-    summation = getSummation((absFun)f3, a, b, n, intensity, nbthreads);
-    break;
-  case 4:
-    summation = getSummation((absFun)f4, a, b, n, intensity, nbthreads);
-    break;
-  default:
-    cerr << "Unknown function id passed: " << functionid << endl;
-  }
-  calcAndDisplayIntegral(summation, a, b, n);
-  calcAndDisplayTime(before_integration);
+  int common_expression = ((b - a) / n);
+  float sum = 0.0;
+  std::chrono::time_point<std::chrono::system_clock> begin = std::chrono::system_clock::now();
+
+  SeqLoop s1;
+
+  s1.parfor<float>(
+      0, n, 1, nbthreads,
+      [&](float &tls) -> void {
+        tls = 0.0;
+      },
+      [&](int i, float &tls) -> void {
+        float x = (a + (i + 0.5) * ((b - a) / n));
+        switch (functionid)
+        {
+        case 1:
+          tls += f1(x, intensity);
+          break;
+        case 2:
+          tls += f2(x, intensity);
+          break;
+        case 3:
+          tls += f3(x, intensity);
+          break;
+        case 4:
+          tls += f4(x, intensity);
+          break;
+        }
+      },
+      [&](float &tls) -> void {
+        sum += tls;
+      });
+
+  std::cout << ((b - a) / n) * sum << endl;
+
+  std::chrono::time_point<std::chrono::system_clock> end = std::chrono::system_clock::now();
+  std::chrono::duration<double> elapsed_seconds = end - begin;
+
+  std::cerr << elapsed_seconds.count() << std::endl;
+
   return 0;
 }
