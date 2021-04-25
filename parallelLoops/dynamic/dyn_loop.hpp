96ac3179941b8368abe2c7ba4cefa1e9f11100a5
devrock20 <satyadev9691@gmail.com>
2021-04-25 12:54:14 -0400

    Adding Dynamic loop

---
+++ parallelLoops/dynamic/dyn_loop.hpp
@@ -0,0 +1,71 @@
+#ifndef __DYN_LOOP_H
+#define __DYN_LOOP_H
+
+#include <functional>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+
+using namespace std;
+class DynLoop
+{
+public:
+  /// @brief execute the function f multiple times with different
+  /// parameters possibly in parallel
+  ///
+  /// f will be executed multiple times with parameters starting at
+  /// beg, no greater than end, in inc increment. These execution may
+  /// be in parallel
+  void parfor1(size_t beg, size_t end, size_t inc,
+               std::function<void(int)> f)
+  {
+    for (size_t i = beg; i < end; i += inc)
+    {
+      f(i);
+    }
+  }
+
+  /// @brief execute the function f multiple times with different
+  /// parameters possibly in parallel
+  ///
+  /// f will be executed multiple times with parameters starting at
+  /// beg, no greater than end, in inc increment. These execution may
+  /// be in parallel.
+  ///
+  /// Each thread that participate in the calculation will have its
+  /// own TLS object.
+  ///
+  /// Each thread will execute function before prior to any
+  /// calculation on a TLS object.
+  ///
+  /// Each thread will be passed the TLS object it executed before on.
+  ///
+  /// Once the iterations are complete, each thread will execute after
+  /// on the TLS object. No two thread can execute after at the same time.
+  template <typename TLS>
+  void parfor(size_t beg, size_t end, size_t increment, size_t nthreads,size_t granularity std::function<void(TLS &)> before,
+              std::function<void(int, TLS &)> func,
+              std::function<void(TLS &)> after)
+  {
+    int number_of_iterations = end/granularity;
+    vector<thread> thread_stack;
+    vector<TLS> threadContextStorage(nthreads);
+    for (int t = 0; t < nthreads; t += 1)
+    {
+      before(threadContextStorage[t]);
+      thread_stack.push_back(thread(&SeqLoop::parfor1, this, t, number_of_iterations, nthreads, [&, t](int j) -> void {
+        func(j, threadContextStorage[t]);
+      }));
+    }
+    for (auto &itr : thread_stack)
+    {
+      itr.join();
+    }
+    for (auto &itr : threadContextStorage)
+    {
+      after(itr);
+    }
+  }
+};
+
+#endif
--- parallelLoops/dynamic/dynamic_sched.cpp
+++ parallelLoops/dynamic/dynamic_sched.cpp
@@ -4,6 +4,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "dyn_loop.hpp"
+#include "vector"
+
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -23,6 +27,56 @@ int main (int argc, char* argv[]) {
     std::cerr<<"usage: "<<argv[0]<<" <functionid> <a> <b> <n> <intensity> <nbthreads> <granularity>"<<std::endl;
     return -1;
   }
+
+  int functionid = stoi(argv[1]);
+  float a = stoi(argv[2]);
+  float b = stoi(argv[3]);
+  float n = stoi(argv[4]);
+  float intensity = stoi(argv[5]);
+  int nbthreads = stoi(argv[6]);
+  int granularity = stoi(argv[7]);
+  int common_expression = ((b - a) / n);
+  float sum = 0.0;
+  std::chrono::time_point<std::chrono::system_clock> begin = std::chrono::system_clock::now();
+
+  float sum = 0.0;
+  std::chrono::time_point<std::chrono::system_clock> begin = std::chrono::system_clock::now();
+
+  DynLoop s1;
+
+  s1.parfor<float>(
+      0, n, 1, nbthreads,granularity,
+      [&](float &tls) -> void {
+        tls = 0.0;
+      },
+      [&](int i, float &tls) -> void {
+        float x = (a + (i + 0.5) * ((b - a) / n));
+        switch (functionid)
+        {
+        case 1:
+          tls += f1(x, intensity);
+          break;
+        case 2:
+          tls += f2(x, intensity);
+          break;
+        case 3:
+          tls += f3(x, intensity);
+          break;
+        case 4:
+          tls += f4(x, intensity);
+          break;
+        }
+      },
+      [&](float &tls) -> void {
+        sum += tls;
+      });
+
+  std::cout << ((b - a) / n) * sum << endl;
+
+  std::chrono::time_point<std::chrono::system_clock> end = std::chrono::system_clock::now();
+  std::chrono::duration<double> elapsed_seconds = end - begin;
+
+  std::cerr << elapsed_seconds.count() << std::endl;
 
   return 0;
 }
