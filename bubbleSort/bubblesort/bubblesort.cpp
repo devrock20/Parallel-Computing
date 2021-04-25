9316860bbb913cefc885c1a015eb8d6dbe457735
Akhil Reddy Gangula <akhilreddy.gangula96@gmail.com>
2021-04-25 16:35:10 -0400

    added bubble sort

---
+++ bubbleSort/bubblesort/bubblesort.cpp
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <iostream>
+#include <unistd.h>
+#include <chrono>
+#include "cmath"
+#include "par_loop.hpp"
+
+using namespace std;
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+  void generateMergeSortData(int *arr, size_t n);
+  void checkMergeSortResult(const int *arr, size_t n);
+
+#ifdef __cplusplus
+}
+#endif
+
+void swap(int *arr, int i, int j)
+{
+  int temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
+void printArr(int *arr, int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    cout << arr[i] << "\t";
+  }
+  cout << endl;
+}
+int main(int argc, char *argv[])
+{
+  if (argc < 3)
+  {
+    std::cerr << "usage: " << argv[0] << " <n> <nbthreads>" << std::endl;
+    return -1;
+  }
+
+  int n = atoi(argv[1]);
+  int nthreads = atoi(argv[2]);
+  // get arr data
+  int *arr = new int[n];
+  generateMergeSortData(arr, n);
+  std::chrono::time_point<std::chrono::system_clock> start = std::chrono::system_clock::now();
+
+  bool swapped = true;
+  while (swapped)
+  {
+    swapped = false;
+    //odd index sorting
+    parfor(
+        1, n - 1, 2, nthreads,
+        [&](int i) -> void {
+          if (arr[i] > arr[i + 1])
+          {
+            swap(arr, i, i + 1);
+            swapped = true;
+          }
+        });
+
+    //even index sorting
+    parfor(
+        0, n - 1, 2, nthreads,
+        [&](int i) -> void {
+          if (arr[i] > arr[i + 1])
+          {
+            swap(arr, i, i + 1);
+            swapped = true;
+          }
+        });
+  }
+
+  std::chrono::time_point<std::chrono::system_clock> end = std::chrono::system_clock::now();
+  std::chrono::duration<double> elpased_seconds = end - start;
+
+  checkMergeSortResult(arr, n);
+  std::cerr << elpased_seconds.count() << std::endl;
+
+  delete[] arr;
+
+  return 0;
+}
--- parallelLoops/dynamic/dynamic_sched.cpp
+++ parallelLoops/dynamic/dynamic_sched.cpp
@@ -1,28 +1,82 @@
+
 #include <iostream>
-#include <chrono>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <chrono>
+#include <cmath>
+#include "seq_loop.hpp"
+#include "vector"
+using namespace std;
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
-float f1(float x, int intensity);
-float f2(float x, int intensity);
-float f3(float x, int intensity);
-float f4(float x, int intensity);
+  float f1(float x, int intensity);
+  float f2(float x, int intensity);
+  float f3(float x, int intensity);
+  float f4(float x, int intensity);
 
 #ifdef __cplusplus
 }
 #endif
 
-int main (int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
 
-  if (argc < 8) {
-    std::cerr<<"usage: "<<argv[0]<<" <functionid> <a> <b> <n> <intensity> <nbthreads> <granularity>"<<std::endl;
+  if (argc < 7)
+  {
+    std::cerr << "usage: " << argv[0] << " <functionid> <a> <b> <n> <intensity> <nbthreads>" << std::endl;
     return -1;
   }
+  int functionid = stoi(argv[1]);
+  float a = stoi(argv[2]);
+  float b = stoi(argv[3]);
+  float n = stoi(argv[4]);
+  float intensity = stoi(argv[5]);
+  int nbthreads = stoi(argv[6]);
+  int granularity = stoi(argv[7]);
+  int common_expression = ((b - a) / n);
+  float sum = 0.0;
+  std::chrono::time_point<std::chrono::system_clock> begin = std::chrono::system_clock::now();
+
+  SeqLoop s1;
+
+  s1.parfor<float>(
+      0, n, 1, nbthreads, granularity,
+      [&](float &tls) -> void {
+        tls = 0.0;
+      },
+      [&](int i, float &tls) -> void {
+        float x = (a + (i + 0.5) * ((b - a) / n));
+        switch (functionid)
+        {
+        case 1:
+          tls += f1(x, intensity);
+          break;
+        case 2:
+          tls += f2(x, intensity);
+          break;
+        case 3:
+          tls += f3(x, intensity);
+          break;
+        case 4:
+          tls += f4(x, intensity);
+          break;
+        }
+      },
+      [&](float &tls) -> void {
+        sum += tls;
+      });
+
+  std::cout << ((b - a) / n) * sum << endl;
+
+  std::chrono::time_point<std::chrono::system_clock> end = std::chrono::system_clock::now();
+  std::chrono::duration<double> elapsed_seconds = end - begin;
+
+  std::cerr << elapsed_seconds.count() << std::endl;
 
   return 0;
 }
