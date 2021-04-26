c57834c24a74fa2d472fbe8da11e98e327f3129c
devrock20 <satyadev9691@gmail.com>
2021-04-26 10:23:29 -0400

    experiment

---
+++ parallelLoops/dynamic/dynamic_sched_1.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <chrono>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "dyn_loop1.hpp"
+
+using namespace std;
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+  float f1(float x, int intensity);
+  float f2(float x, int intensity);
+  float f3(float x, int intensity);
+  float f4(float x, int intensity);
+
+#ifdef __cplusplus
+}
+
+int main (int argc, char* argv[]) {
+
+  if (argc < 8) {
+    std::cerr<<"usage: "<<argv[0]<<" <functionid> <a> <b> <n> <intensity> <nbthreads> <granularity>"<<std::endl;
+    return -1;
+  }
+
+  int functionid = stoi(argv[1]);
+  float a = stoi(argv[2]);
+  float b = stoi(argv[3]);
+  float n = stoi(argv[4]);
+  float intensity = stoi(argv[5]);
+  int nbthreads = stoi(argv[6]);
+  int granularity = stoi(argv[7]);
+  int chunk_size = n/granularity;
+  int common_expression = ((b - a) / n);
+  float sum = 0.0;
+
+  std::chrono::time_point<std::chrono::system_clock> begin = std::chrono::system_clock::now();
+
+  DynLoop d1;
+  vector<thread> thread_pool;
+  for (int t = 0; t < nthreads; t += 1){
+    thread_stack.push_back(thread(&DynLoop::parfor, this,[&](int i, float &tls) -> void {
+        float x = (a + (i + 0.5) * ((b - a) / n));
+        switch (functionid)
+        {
+        case 1:
+          tls += f1(x, intensity);
+          break;
+        case 2:
+          tls += f2(x, intensity);
+          break;
+        case 3:
+          tls += f3(x, intensity);
+          break;
+        case 4:
+          tls += f4(x, intensity);
+          break;
+        }
+      }));
+  }
+
+  std::cout << ((b - a) / n) * sum << endl;
+
+  std::chrono::time_point<std::chrono::system_clock> end = std::chrono::system_clock::now();
+  std::chrono::duration<double> elapsed_seconds = end - begin;
+
+  std::cerr << elapsed_seconds.count() << std::endl;
+
+  return 0;
+}
