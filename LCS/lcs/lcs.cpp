bc656d1cbe2933db07a2bfd92af03cf4cef92cbe
Akhil Reddy Gangula <akhilreddy.gangula96@gmail.com>
2021-04-30 01:18:42 -0400

    checkpoint: LCS

--- LCS/lcs/lcs.cpp
+++ LCS/lcs/lcs.cpp
@@ -4,68 +4,181 @@
 #include <fcntl.h>
 #include <iostream>
 #include <unistd.h>
-
-
+#include <chrono>
+#include "cmath"
+#include "par_loop.hpp"
+using namespace std;
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
-  void generateLCS(char* X, int m, char* Y, int n);
-  void checkLCS(char* X, int m, char* Y, int n, int result);
+  void generateLCS(char *X, int m, char *Y, int n);
+  void checkLCS(char *X, int m, char *Y, int n, int result);
 
 #ifdef __cplusplus
 }
 #endif
 
+int LCS(char *X, int m, char *Y, int n, int nthreads)
+{
 
+  int **C = new int *[m + 1];
+  for (int i = 0; i <= m; ++i)
+  {
+    C[i] = new int[n + 1];
+    C[i][0] = 0;
+  }
+  for (int j = 0; j <= n; ++j)
+  {
+    C[0][j] = 0;
+  }
 
-int main (int argc, char* argv[]) {
+  // for (int i = 0; i < m; i++)
+  // {
+  //   cout << X[i] << "\t";
+  // }
+  // cout << endl;
+  // for (int j = 0; j < m; j++)
+  // {
+  //   cout << Y[j] << "\t";
+  // }
 
-  if (argc < 4) { std::cerr<<"usage: "<<argv[0]<<" <m> <n> <nbthreads>"<<std::endl;
+  // cout << endl;
+  auto func = [&](int i, int j) -> void {
+    if (X[i] == Y[j])
+    {
+      // cout << i << "|" << j << " +1" << endl;
+
+      C[i + 1][j + 1] = C[i][j] + 1;
+    }
+    else
+    {
+      // cout << i << "|" << j << " max";
+      C[i + 1][j + 1] = std::max(C[i][j + 1], C[i + 1][j]);
+    }
+  };
+  for (int k = 1; k < m; k++)
+  {
+    // int i = k, j = 1;
+    // staticFor(
+    //     0, 0, [&]() -> bool { return i > 0 && j < n; }, 0, nthreads, [&]() -> void {
+    //   func(i, j);
+    //   i--; });
+    // int i = k, j = 1;
+    parfor(
+        k, 1,
+        [&](int i, int j) -> bool { return i > 0 && j < n; },
+        [](int i, int j) -> void { i--, j++; },
+        [&](int i, int j) -> void {
+          func(i, j);
+          //             for (int i = 0; i <= m; i++)
+          // {
+          //   for (int j = 0; j <= n; j++)
+          //   {
+          //     cout << C[i][j] << "\t";
+          //   }
+          //   cout << endl;
+          // }
+        },
+        nthreads);
+    // for (int i = k, j = 1;i > 0 && j < n; ; i--, j++)
+    // {
+    //   func(i, j);
+    // }
+    // cout << endl;
+    // cout << "row number" << k << endl;
+  }
+  // cout << endl;
+
+  // cout << "phase 2" << endl;
+  for (int c = 1; c < n; c++)
+  {
+       parfor(
+        m-1, c,
+        [&](int i, int j) -> bool { return i > 0 && j < n; },
+        [](int i, int j) -> void { i--, j++; },
+        [&](int i, int j) -> void {
+          func(i, j);
+        },
+        nthreads);
+    // for (int j = c, i = m - 1; j < n && i > 0; j++, i--)
+    // {
+// func(i,j);
+      
+      // if (X[i] == Y[j])
+      // {
+      //   cout << i << "|" << j << " +1" << endl;
+
+      //   C[i + 1][j + 1] = C[i][j] + 1;
+      // }
+      // else
+      // {
+      //   // cout << i << "|" << j << " max";
+      //   C[i + 1][j + 1] = std::max(C[i][j + 1], C[i + 1][j]);
+      // }
+    // }
+    // cout << endl;
+    // cout << "column number" << c << endl;
+  }
+
+  int result = C[m][n];
+  // cout << "Finish" << result << endl;
+  // for (int i = 0; i <= m; i++)
+  // {
+  //   for (int j = 0; j <= n; j++)
+  //   {
+  //     cout << C[i][j] << "\t";
+  //   }
+  //   cout << endl;
+  // }
+
+  // cout << "Rsult" << m-1 << n-1 << "\t" << C[m-1][n-1] << endl;
+
+  for (int i = 0; i <= m; ++i)
+  {
+    delete[] C[i];
+  }
+  delete[] C;
+
+  return result;
+}
+
+int main(int argc, char *argv[])
+{
+
+  if (argc < 4)
+  {
+    std::cerr << "usage: " << argv[0] << " <m> <n> <nbthreads>" << std::endl;
     return -1;
   }
 
   int m = atoi(argv[1]);
   int n = atoi(argv[2]);
+  int nthreads = atoi(argv[3]);
 
-  // get string data 
+  // get string data
   char *X = new char[m];
   char *Y = new char[n];
   generateLCS(X, m, Y, n);
-  int** C = new int*[m+1];
-  for (int i=0; i<=m; ++i) {
-    C[i] = new int[n+1];
-    C[i][0] = 0;
-  }
-  for (int j=0; j<=n; ++j) {
-    C[0][j] = 0;
-  }
-  
-int Lmax=0;
-int count =0;
-for(int i;i<=m;i++) {
-	for(int j=1;j<=i;j++) {
-		int k=i-(j-1);
-		if(Y[k-1]==X[j-1]) {
-			C[k][j]=C[k-1][j-1]+1;
-			if(C[k][j]>Lmax) {
-				Lmax=C[k][j];
-				// parent[s]=k-1;
-				// s++;
-				count++;
-			}
-		} else if(C[k-1][j]>=C[k][j-1]) {
-			C[k][j]=C[k-1][j];
-		} else {
-			C[k][j]=C[k][j-1];
-		}
-	}
-}
-  int result = -1; // length of common subsequence
 
-result = Lmax;
-  checkLCS(X, m, Y, n, result);
+  // cout << endl;
+  //insert LCS code here.
+  std::chrono::time_point<std::chrono::system_clock> start = std::chrono::system_clock::now();
 
+  int lcs = LCS(X, m, Y, n, nthreads);
+  // cout << lcs << "Resultasdasd" << endl;
+  std::chrono::time_point<std::chrono::system_clock> end = std::chrono::system_clock::now();
+  std::chrono::duration<double> elpased_seconds = end - start;
+
+  // checkLCS(X, m, Y, n, lcs);
+  std::cerr << elpased_seconds.count() << std::endl;
+
+  delete[] X;
+  delete[] Y;
+
+  // int result = -1; // length of common subsequence
+  checkLCS(X, m, Y, n, lcs);
 
   return 0;
 }
---
+++ LCS/lcs/par_loop.hpp
@@ -0,0 +1,131 @@
+#ifndef __SEQ_LOOP_H
+#define __SEQ_LOOP_H
+
+#include <functional>
+#include <vector>
+#include <thread>
+#include <queue>
+#include <iostream>
+#include <condition_variable>
+using namespace std;
+
+struct Task
+{
+  int i, j;
+};
+
+//create a queue that holds the tasks
+queue<Task> tasks;
+std::condition_variable cv;
+std::mutex cv_m;
+bool done = false;
+
+//executor which picks the task in the queue and runs it.
+void executor(function<void(int i, int j)> func)
+{
+  while (true)
+  {
+    Task task;
+    {
+      unique_lock<mutex> lk(cv_m);
+      cv.wait(lk, [&] { return !tasks.empty() || done; });
+      if (done && tasks.empty())
+      {
+        break;
+      }
+      task = tasks.front();
+      tasks.pop();
+    }
+    func(task.i, task.j);
+  }
+}
+
+void parfor(size_t beg1, size_t beg2,
+            std::function<bool(int, int)> condt,
+            std::function<void(int, int)> post,
+            std::function<void(int, int)> func,
+            size_t nthreads)
+{
+
+  //create thread pool
+  vector<thread> thread_stack;
+  bool sorted = false;
+  //start executor parallely
+  for (int t = 0; t < nthreads; t += 1)
+  {
+    thread_stack.push_back(thread(executor, func));
+    // cout << "Started: thread: " << t << endl;
+  }
+  //start pushing task to queue
+  for (int i = beg1, j = beg2; condt(i, j); i--,j++)
+  {
+// cout << i << "|" <<j <<endl;
+    {
+      lock_guard<mutex> lock(cv_m);
+      tasks.push(Task{i, j});
+      // cout<< "Pushed: " << i <<endl;
+    }
+    //notify that task is available
+    cv.notify_one();
+  }
+
+  // all task pushed mark main task as done and wait for threads to complete them
+  done = true;
+  cv.notify_all();
+  for (auto &itr : thread_stack)
+  {
+    itr.join();
+  }
+}
+void simpleFor(size_t beg, size_t end, size_t inc,
+               std::function<void(int)> f)
+{
+  for (size_t i = beg; i < end; i += inc)
+  {
+    f(i);
+  }
+}
+
+// void staticFor(size_t beg, size_t end, std::function<bool()> cndt, size_t inc, size_t nthreads,
+//                std::function<void()> func)
+// {
+//   vector<thread> thread_stack;
+//   for (; cndt();)
+//   {
+//     thread_stack.push_back(thread(func));
+//   }
+//   for (int t = 1, start = beg; t <= nthreads; t++, start += inc)
+//   {
+//     thread_stack.push_back(thread(simpleFor, start, end, inc * nthreads, func));
+//   }
+//   for (auto &itr : thread_stack)
+//   {
+//     itr.join();
+//   }
+// }
+
+// template <typename TLS>
+// void parfor(size_t beg, size_t end, size_t increment, size_t nthreads, std::function<void(TLS &)> before,
+//             std::function<void(int, TLS &)> func,
+//             std::function<void(TLS &)> after)
+// {
+//   vector<thread> thread_stack;
+//   vector<TLS> threadContextStorage(nthreads);
+//   for (int t = 0; t < nthreads; t += 1)
+//   {
+//     before(threadContextStorage[t]);
+//     thread_stack.push_back(thread(simpleFor, t, end, nthreads, [&, t](int j) -> void {
+//       func(j, threadContextStorage[t]);
+//     }));
+//   }
+//   for (auto &itr : thread_stack)
+//   {
+//     itr.join();
+//   }
+//   for (auto &itr : threadContextStorage)
+//   {
+//     after(itr);
+//   }
+// }
+
+#endif
