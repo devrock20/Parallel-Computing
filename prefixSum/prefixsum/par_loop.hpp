e391bf631205107d1af1b7be4f908ab88ba8e135
akhilgangula <58249928+akhilgangula@users.noreply.github.com>
2021-05-09 09:56:07 -0400

    Update par_loop.hpp

--- prefixSum/prefixsum/par_loop.hpp
+++ prefixSum/prefixsum/par_loop.hpp
@@ -19,22 +19,14 @@ public:
     nbthread = t;
   }
 
-  // template<typename TLS>
   void parfor (size_t beg, size_t end, size_t increment, std::function<void(int)> f) {
-#pragma omp parallel num_threads(nbthread)
-    {
-      // TLS tls;
-      // before(tls);
-      
-#pragma omp for schedule(static) 
-      for (size_t i=beg; i<end; i+= increment) {
-	f(i);
-      }
-// #pragma omp critical
-      // after(tls);
-    }
+	#pragma omp parallel num_threads(nbthread) {
+	#pragma omp for schedule(static) 
+	for (size_t i=beg; i<end; i+= increment) {
+		f(i);
+	}
+     }
   }
-
 };
 
 #endif
